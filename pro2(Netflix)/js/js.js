b4e4f3a09002b2006c9c72703b017a190bf741b6
robson120-s <shroba391@gmail.com>
2025-04-11 23:18:22 +0300

    second-finishing

--- pro2(Netflix)/js/js.js
+++ pro2(Netflix)/js/js.js
@@ -1,8 +1,40 @@
 
 $(document).ready(function(){
   $('.one').click(function(){
-    $('.jq1').fadeToggle(800);
+    $('.jq1').fadeToggle(600);
   });
 });
 
 
+$(document).ready(function(){
+  $('.two').click(function(){
+    $(".jq2").toggle(200);
+  });
+});
+
+
+$(document).ready(function(){
+  $('.three').click(function(){
+    $('.jq3').toggle(1000);
+  });
+});
+
+
+$(document).ready(function(){
+  $('.four').click(function(){
+    $('.jq4').toggle(1000);
+  });
+});
+
+$(document).ready(function(){
+  $('.five').click(function(){
+    $('.jq5').toggle(1000);
+  });
+});
+
+
+$(document).ready(function(){
+  $('.six').click(function(){
+    $('.jq6').toggle(1000);
+  });
+});
